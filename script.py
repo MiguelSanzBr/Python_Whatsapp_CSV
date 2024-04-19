b685220fc3273e08b12bb8c2273382d5a7bce73b
miguelsanzbr <miguelsanzb.p@gmail.com>
2024-04-19 19:42:26 -0300

    finish day

--- script.py
+++ script.py
@@ -6,10 +6,12 @@ import pyautogui
 import time
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+import sys
 
 # NOMES DAS COLUNAS
 ########################
-colMsg,colTel= 'menssage','phone' 
+colMsg = 'FRASE'
+colTel= 'TEL' 
 ######################
 
 def validNumber(phone):
@@ -18,11 +20,11 @@ def validNumber(phone):
         return phonenumbers.is_valid_number(pNum) and pNum.country_code == 55
     except phonenumbers.NumberParseException:
         return False
-    
 def enviarmsg(phone, menssage):
     if validNumber('+55' + str(phone)):
         #time.sleep(random.randint(50,120))
         pwk.sendwhatmsg_instantly('+55' + str(phone), menssage)
+        time.sleep(12)
         pyautogui.hotkey("ctrl", "w")
         pyautogui.press("enter")
         print(f"mensagem enviado a {phone}: {menssage}")
@@ -33,24 +35,18 @@ def enviarmsg(phone, menssage):
         hora = f"{now.tm_hour}:{now.tm_min}"
         with open('ERRO_NUMBER_DB.txt', 'a') as f:
             f.write(f"{phone} \n")
-
-        print(phone,"não é valido")    
-
-
+        print(phone,"error 404")   
+    
 def lercsv(archivo):
     df = pd.read_csv(archivo)
     columnas = df.columns.tolist()
     if colTel in columnas and colMsg in columnas:
         tell = df[colTel].tolist()
         msg = df[colMsg].tolist()
-
         for phone, menssage in zip(tell, msg):
             enviarmsg(phone, menssage)
-            
-            
     else:
-        print("O arquivo CSV não possui as colunas 'telefone' e 'mensagem'.")
-
+        print(f"O arquivo CSV não possui as colunas '{colTel}' e '{colMsg}'.")
 
 Tk().withdraw()
 archivo_csv = askopenfilename(filetypes=[('Archivos CSV', '*.csv')])
