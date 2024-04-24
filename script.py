48a5136dd0b1d27b8de17967203ead77e46b425c
miguelsanzbr <miguelsanzb.p@gmail.com>
2024-04-24 18:59:22 -0300

    selenium init verify user

--- script.py
+++ script.py
@@ -6,6 +6,13 @@ import pyautogui
 import time
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.firefox import GeckoDriverManager
+# from selenium.webdriver.firefox.service import Service
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 # NOMES DAS COLUNAS
 ########################
@@ -47,10 +54,33 @@ def lercsv(archivo):
     else:
         print(f"O arquivo CSV não possui as colunas '{colTel}' e '{colMsg}'.")
 
+def verifyLogin(driver):
+    driver.get('https://web.whatsapp.com/')
+    try:
+        time.sleep(12)
+        driver.find_element(By.XPATH, "//canvas[@aria-label='Scan me!']")
+        print("Não está logado")
+        print("Escaneie o QR Code,então precione Enter")
+        input()
+        return False
+    except NoSuchElementException:
+        return True
+
+
+
 Tk().withdraw()
 archivo_csv = askopenfilename(filetypes=[('Archivos CSV', '*.csv')])
 
 if archivo_csv:
+    servico = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=servico)
+    
+    # FIRE FOX 
+    # servico = Service(GeckoDriverManager().install())
+    # driver = webdriver.Firefox(service=servico)
+    
+    while not verifyLogin(driver):
+        pass
     lercsv(archivo_csv)
 else:
     print("Nenhum arquivo foi selecionado.")
